32fc17061a6305a220a34eaa34ca8798cd2fd45d
montellese <montellese@xbmc.org>
2012-01-26 15:42:52 +0100

    replace "Network" through "Services" in settings

--- xbmc/input/ButtonTranslator.cpp
+++ xbmc/input/ButtonTranslator.cpp
@@ -236,7 +236,7 @@ static const ActionMapping windows[] =
         {"musicsettings"            , WINDOW_SETTINGS_MYMUSIC},
         {"systemsettings"           , WINDOW_SETTINGS_SYSTEM},
         {"videossettings"           , WINDOW_SETTINGS_MYVIDEOS},
-        {"networksettings"          , WINDOW_SETTINGS_NETWORK},
+        {"servicesettings"          , WINDOW_SETTINGS_SERVICES},
         {"appearancesettings"       , WINDOW_SETTINGS_APPEARANCE},
         {"scripts"                  , WINDOW_PROGRAMS}, // backward compat
         {"videofiles"               , WINDOW_VIDEO_FILES},
