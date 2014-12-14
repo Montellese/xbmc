7799fbcf3bf83493be219fad5c83152578ad38cc
montellese <montellese@xbmc.org>
2014-12-14 23:27:23 +0100

    [database] register CVideoImportRepository and deinitialize databases on application exit to disable remote items

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2558,6 +2558,8 @@ bool CApplication::Cleanup()
     if (m_pGUI)
       m_pGUI->GetWindowManager().DestroyWindows();
 
+    CServiceBroker::GetDatabaseManager().Deinitialize();
+
     CLog::Log(LOGINFO, "unload sections");
 
     //  Shutdown as much as possible of the
