f7f84935a361f2541939f7a2a71d2084770cf170
montellese <montellese@xbmc.org>
2014-12-14 23:27:23 +0100

    database: register CVideoImportRepository and deinitialize databases on application exit to disable remote items

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2882,6 +2882,8 @@ bool CApplication::Cleanup()
     CLog::Log(LOGNOTICE, "closing down remote control service");
     CInputManager::GetInstance().DisableRemoteControl();
 
+    CDatabaseManager::GetInstance().Deinitialize();
+
     CLog::Log(LOGNOTICE, "unload sections");
 
 #ifdef HAS_PERFORMANCE_SAMPLE
