b1203e1c5d4db6a438769239c0a446277aa1ffdb
Montellese <montellese@kodi.tv>
2016-10-25 17:28:03 +0200

    [network] add support for broadcast UDP sockets

--- xbmc/network/Socket.h
+++ xbmc/network/Socket.h
@@ -210,17 +210,19 @@ namespace SOCKETS
     bool IsIp4vOnly() const { return m_ipv4Only; }
     void SetIpv4Only(bool ipv4Only) { m_ipv4Only = ipv4Only; }
 
+    bool IsBroadcast() const { return m_broadcast; }
+    void SetBroadcast(bool broadcast) { m_broadcast = broadcast; }
+
     // I/O functions
     virtual int SendTo(const CAddress& addr, const int bufferlength,
                        const void* buffer) = 0;
 
     // read datagrams, return no. of bytes read or -1 or error
     virtual int  Read(CAddress& addr, const int buffersize, void *buffer) = 0;
-    virtual bool Broadcast(const CAddress& addr, const int datasize,
-                           const void* data) = 0;
 
   protected:
     bool m_ipv4Only;
+    bool m_broadcast;
   };
 
   // Implementation specific classes
@@ -242,11 +244,6 @@ namespace SOCKETS
     bool Listen(int timeout);
     int  SendTo(const CAddress& addr, const int datasize, const void* data);
     int  Read(CAddress& addr, const int buffersize, void *buffer);
-    bool Broadcast(const CAddress& addr, const int datasize, const void* data)
-    {
-      //! @todo implement
-      return false;
-    }
     SOCKET  Socket() { return m_iSock; }
     void Close();
 
