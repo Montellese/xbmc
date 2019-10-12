016b4f4003ccf30f9f5d43c650f4ccd6b42bd450
montellese <montellese@kodi.tv>
2019-10-13 00:39:57 +0200

    [CAddonMediaImporter] fully integrate CMediaImportAddonManager

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -18,6 +18,7 @@
 #include "PlayListPlayer.h"
 #include "URL.h"
 #include "Util.h"
+#include "addons/MediaImporter.h"
 #include "addons/Skin.h"
 #include "addons/VFSEntry.h"
 #include "cores/AudioEngine/Engines/ActiveAE/ActiveAE.h"
@@ -901,6 +902,7 @@ bool CApplication::Initialize()
 
     // start media import
     CServiceBroker::GetMediaImportManager().Initialize(&databaseManager);
+    CServiceBroker::GetMediaImportAddons().Start();
   }
 
   CLog::Log(LOGINFO, "initialize done");
@@ -2704,6 +2706,7 @@ void CApplication::Stop(int exitCode)
     m_ExitCode = exitCode;
     CLog::Log(LOGINFO, "Stopping all");
 
+    CServiceBroker::GetMediaImportAddons().Stop();
     CServiceBroker::GetMediaImportManager().Uninitialize();
 
     // cancel any jobs from the jobmanager
@@ -2742,6 +2745,7 @@ void CApplication::Stop(int exitCode)
 #endif
 
     // stop media import
+    CServiceBroker::GetMediaImportAddons().Stop();
     CServiceBroker::GetMediaImportManager().Uninitialize();
 
     // Stop services before unloading Python
