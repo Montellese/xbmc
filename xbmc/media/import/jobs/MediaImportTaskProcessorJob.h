ada70176a58d7796fde458b90743874c7b3db165
montellese <montellese@kodi.tv>
2019-12-28 22:25:08 +0100

    [media import] add structure and logic for importing media items

---
+++ xbmc/media/import/jobs/MediaImportTaskProcessorJob.h
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (C) 2021 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "LibraryQueue.h"
+#include "media/import/MediaImportSource.h"
+#include "utils/logtypes.h"
+
+#include <memory>
+#include <string>
+
+class CGUIDialogProgressBarHandle;
+class IMediaImportTask;
+class IMediaImportTaskCallback;
+
+class CMediaImportTaskProcessorJob : public CLibraryJob
+{
+public:
+  ~CMediaImportTaskProcessorJob() override;
+
+  const CMediaImportSource& GetSource() const { return m_source; }
+
+  std::shared_ptr<const IMediaImportTask> GetCurrentTask() const { return m_task; }
+
+  /*!
+   * \brief Get the progress bar handle instance used by the import task
+   */
+  CGUIDialogProgressBarHandle* GetProgressBarHandle(const std::string& title = "");
+
+  // implementation of CJob
+  bool DoWork() override;
+  bool operator==(const CJob* job) const override;
+
+protected:
+  CMediaImportTaskProcessorJob(const CMediaImportSource& source,
+                               IMediaImportTaskCallback* callback,
+                               bool hasProgress);
+
+  void SetTask(std::shared_ptr<IMediaImportTask> task);
+  void ResetTask();
+
+  virtual bool ProcessTask(std::shared_ptr<IMediaImportTask> task);
+  virtual bool OnTaskComplete(bool success, const IMediaImportTask* task);
+
+  static Logger GetLogger();
+
+  CMediaImportSource m_source;
+  IMediaImportTaskCallback* m_callback;
+  std::shared_ptr<IMediaImportTask> m_task;
+  bool m_hasProgress;
+  CGUIDialogProgressBarHandle* m_progress;
+};
